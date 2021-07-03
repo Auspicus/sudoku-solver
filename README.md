4da8796bd289a042495b68be18a7fe561b40da1a
Auspicus <elijahdisch@gmail.com>
2021-07-03 23:32:00 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-5mw1hx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-5mw1hx)
---
+++ index.ts
@@ -0,0 +1,58 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+[
+  [0x0,0x1,0x2,0x3],
+  [0x4,0x5,0x6,0x7],
+  [0x8,0x9,0xa,0xb],
+  [0xc,0xd,0xe,0xf],
+]
+
+const solve = (board: SudokuBoard) => {
+  const depthMap = new Map()
+
+}
+
+class SudokuBoard {
+  board: number[][]
+  width: number
+  height: number
+  subdivisionWidth: number
+  subdivisionHeight: number
+
+  constructor(boardMatrix: number[][]) {
+    this.board = boardMatrix
+    this.width = boardMatrix.length
+    this.height = boardMatrix.length
+    this.subdivisionWidth = this.width / 2
+    this.subdivisionHeight = this.height / 2
+  }
+
+  valueAt(x, y) {
+    return this.board[y][x]
+  }
+
+  getRow(y: number) {
+    const set = new Set()
+    for (let i = 0; i < this.width; i++) {
+      set.add(this.valueAt(i, y))
+    }
+    return set
+  }
+
+  getColumm(x: number) {
+    const set = new Set()
+    for (let i = 0; i < this.height; i++) {
+      set.add(this.valueAt(x, i))
+    }
+    return set
+  }
+
+  getSquare(x, y) {
+   
+  }
+}
